14ee10fcb09de2697b528ce7ba293c0152960e2e
Shawn Ren <shawnren527@gmail.com>
2019-04-23 02:21:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -35,7 +35,7 @@ class App < Sinatra::Base
     result
   end
 
-  get '/:operation/:number1/:number2'
+  get '/:operation/:number1/:number2' do
    number1 = params[:number1].to_i
    number2 = params[:number2].to_i
    operation = params[:operation]
