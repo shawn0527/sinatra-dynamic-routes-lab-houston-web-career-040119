a3198bc017be591d2ea0d1c2098890e1f7165efb
Shawn Ren <shawnren527@gmail.com>
2019-04-23 02:30:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -38,7 +38,7 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
    number1 = params[:number1].to_i
    number2 = params[:number2].to_i
-   operation = params[:operation]
+   case params[:operation]
    if operation == 'add'
      (number1 + number2).to_s
    elsif opertion == 'subtract'
