7a6b6df53c5d6927725a5861eaab5f51a130f954
Shawn Ren <shawnren527@gmail.com>
2019-04-23 02:34:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -41,7 +41,7 @@ class App < Sinatra::Base
    operation = params[:operation]
    if operation == 'add'
      (number1 + number2).to_s
-   elsif 'subtract'
+   elsif operation == 'subtract'
      (number1 - number2).to_s
    elsif 'multiply'
      (number1 * number2).to_s
